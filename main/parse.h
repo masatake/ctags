05eb83539b47a095a90aea3a59b9fdb28e4a6ba3
Masatake YAMATO <yamato@redhat.com>
2020-11-29 05:14:37 +0900

    main: export getLanguageKindForName to parsers
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- main/parse.h
+++ main/parse.h
@@ -147,6 +147,7 @@ extern bool isLanguageKindEnabled (const langType language, int kindIndex);
 extern bool isLanguageRoleEnabled (const langType language, int kindIndex, int roleIndex);
 
 extern kindDefinition* getLanguageKindForLetter (const langType language, char kindLetter);
+extern kindDefinition* getLanguageKindForName (const langType language, const char *kindName);
 
 extern void initializeParser (langType language);
 
